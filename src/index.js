8b35b89c65642223698987244f740fe61487155d
Ostap <Ostapkriven13@gmail.com>
2023-10-10 20:22:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = document.getElementById('root');
 const reactRoot = createRoot(root);
 
 reactRoot.render(
-  <BrowserRouter>
+  <BrowserRouter basename='goit-react-hw-05-movies/'>
     <App />
   </BrowserRouter>
 );
