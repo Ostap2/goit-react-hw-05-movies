20fe106f20be664d24c25dae826ba459402bdaa8
Ostap <Ostapkriven13@gmail.com>
2023-10-15 20:20:28 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,6 @@
-// import React from 'react';
-
-// const Header = () => {
-//   return (
-    // <header className='header'>
-    //   <div className='nav-container'>
-    //     <nav>
-    //       <ul className='list'>
-    //         <li className='item-nav'>
-    //           <a href="/" className='link-nav'>Home</a>
-    //         </li >
-    //         <li className='item-nav'>
-    //           <a href="/movies" className='link-nav'>Movies</a>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
-    // </header>
-//   );
-// };
-
-// export default Header;
-
-
-
-
 
 import React, { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import {Outlet } from 'react-router-dom';
 
 const Header = () => {
   return (
