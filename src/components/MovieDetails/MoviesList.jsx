d3a88843941a316330ac905213a6ddb5179844c2
Ostap <Ostapkriven13@gmail.com>
2023-10-18 21:09:55 +0300

    і
    і

--- src/components/MovieDetails/MoviesList.jsx
+++ src/components/MovieDetails/MoviesList.jsx
@@ -4,15 +4,17 @@ export const ProductList = ({ products }) => {
   const location = useLocation();
 
   return (
-<Link to={`/movies/${movie.id}`} state={{ from: location }}>
-               (
-                <img
-                  src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-                  alt={movie.title}
-                  className="img-films"
-                />
-              )
-              {movie.title}
-            </Link>
+    <div>
+      {products.map((movie) => (
+        <Link to={`/movies/${movie.id}`} state={{ from: location }} key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+            alt={movie.title}
+            className="img-films"
+          />
+          {movie.title}
+        </Link>
+      ))}
+    </div>
   );
 };
